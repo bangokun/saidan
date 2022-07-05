33de9709a0ae422703d6a712efffcb06803f37d6
bangokun <108464869+bangokun@users.noreply.github.com>
2022-07-05 16:03:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['6285738639969'],
+  ['6285738639969'],
+  ['6285738639969', 'gempy', true]  
   
 ] 
-global.ownername = [['Gempy']]
-global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.ownername = [['Agus']]
+global.namabot = [['Crash-MD']]
+global.ownerinsta = [['@Agus']]
 global.mods = [] 
 global.prems = [] 
 
@@ -61,7 +61,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Created by Gempy'
+global.wm = 'Created by Crash'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
